3f6de29a357a02caa335ddcac09c89eb4f81c902
JMcQuillanMBAir <johan.mcquillan.13@ucl.ac.uk>
2018-04-20 14:00:43 +0100

    Restructure tax calculations into functions
    Elaborate comments and streamline printin routine

--- simple_taxer.py
+++ simple_taxer.py
@@ -1,53 +1,69 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
+import sys
+
+# Income tax brackets and rates
+income_tax = [[ 11850,  46350,  150000],
+              [  0.20,   0.40,    0.45]]
+
+# National insurance brackets and rates
+nation_ins = [[162*52,  892*52],
+              [  0.12,    0.02]]
+
+rate_student = 0.09     # Rate of student loan repayments
+thres_student = 25000.  # Threshold before student loan repayments
+
+def get_income_tax(salary):
+    untaxed = salary
+    tax = 0.
+    for i in reversed(range(len(income_tax))):
+        if salary > income_tax[0][i]:
+            # Apply tax rate to amount within bracket
+            tax += (untaxed - income_tax[0][i]) * income_tax[1][i]
+            margin = income_tax[0][i]
+    return tax
+
+def get_ni(salary):
+    uninsured = salary
+    insurance = 0.
+    for i in reversed(range(len(nation_ins))):
+        if salary > nation_ins[0][i]:
+            # Apply nation insurance rate to amount within bracket
+            insurance += (uninsured - nation_ins[0][i]) * nation_ins[1][i]
+            uninsured = nation_ins[0][i]
+    return insurance
 
 parser = ArgumentParser()
 parser.add_argument('gross', type=float, help='Gross income')
 parser.add_argument('--no-student', dest='student', action='store_false', help='Has no student loan')
 args = parser.parse_args()
 
-rate_student = 0.09
-thres_student = 25000
-
-income_tax = [[ 11850,  46350,  150000],
-              [  0.20,   0.40,    0.45]]
-
-nation_ins = [[702*52, 52*3863],
-              [  0.12,    0.02]]
-
-i = 0
 gross = args.gross
 total = gross
+output = [('Gross Income', gross)]
 
-taxed = 0
-to_tax = gross
-for i in reversed(range(len(income_tax))):
-    if gross > income_tax[0][i]:
-        taxed += (to_tax - income_tax[0][i]) * income_tax[1][i]
-        to_tax = income_tax[0][i]
-total -= taxed
+tax = get_income_tax(gross)
+total -= tax
+output.append(('Income Tax', -tax))
 
-insured = 0
-to_insure = gross
-for i in reversed(range(len(nation_ins))):
-    if gross > nation_ins[0][i]:
-        insured += (to_insure - nation_ins[0][i]) * nation_ins[1][i]
-        to_insure = nation_ins[0][i]
-total -= insured
+ni = get_ni(gross)
+total -= ni
+output.append(('National Ins.', -ni))
 
-if gross > thres_student:
-    student_fee = (gross - thres_student) * rate_student
-    total -= student_fee
-else:
-    student_fee = 0
-
-
-print '{:>15}:   {:>12,.2f}'.format('Gross Income', gross)
-print '{:>15}:   {:>12,.2f}'.format('Income Tax', -taxed)
-print '{:>15}:   {:>12,.2f}'.format('National Ins.', -insured)
 if args.student:
-    print '{:>15}:   {:>12,.2f}'.format('Student Loan', -student_fee)
-print '{:>15}:   {:>12,.2f}'.format('Total', total)
-print '{:>15}:   {:>12,.2f}'.format('Monthly Pay', total/12)
+    slr = (gross - thres_student) * rate_student
+    output.append(('Student Loan', -slr))
+else:
+    slr = 0.
+total -= slr
+
+output.append(('Total', total))
+output.append(('Monthly Pay', total/12.))
+output.append(('Weekly Pay', total/52.))
+
+for name, money in output:
+    sys.stdout.write('{:>15}:   {:>12,.2f}\n'.format(name, money))
+sys.stdout.flush()
+
 

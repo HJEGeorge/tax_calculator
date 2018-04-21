d902b673f582581aa9d39dd8b67785a046a5e62c
JMcQuillanMBAir <johan.mcquillan.13@ucl.ac.uk>
2018-04-21 07:39:06 +0100

    Align +/- sign

--- tax.py
+++ tax.py
@@ -13,28 +13,27 @@ args = parser.parse_args()
 
 gross = args.gross
 total = gross
-output = [('Gross Income', gross)]
+output = [('Gross Income', ' ', gross)]
 
 it = IncomeTax.tax(gross)
 total -= it
-print it, type(it)
-output.append(('Income Tax', -it))
+output.append(('Income Tax', '-', it))
 
 ni = NationalInsurance.tax(gross)
 total -= ni
-output.append(('National Ins.', -ni))
+output.append(('National Ins.', '-', ni))
 
 if args.student:
     slr = StudentFinance.tax(gross)
-    output.append(('Student Loan', -slr))
+    output.append(('Student Loan', '-', slr))
 else:
     slr = 0.
 total -= slr
 
-output.append(('Total', total))
-output.append(('Monthly Pay', total/12.))
-output.append(('Weekly Pay', total/52.))
+output.append(('Total', ' ', total))
+output.append(('Monthly Pay', ' ', total/12.))
+output.append(('Weekly Pay', ' ', total/52.))
 
-for name, money in output:
-    sys.stdout.write('{:>15}:   {:>12,.2f} £\n'.format(name, money))
+for name, sign, money in output:
+    sys.stdout.write('{:>15}:   {}{:>12,.2f} £\n'.format(name, sign, money))
 sys.stdout.flush()

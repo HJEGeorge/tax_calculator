532ec7053962fbfffef220dc7678a92eed5ab60f
JMcQuillanMBAir <johan.mcquillan.13@ucl.ac.uk>
2018-04-23 12:56:24 +0100

    Add option to display tax as percentage of gross

--- tax_sag.py
+++ tax_sag.py
@@ -19,20 +19,26 @@ args = parser.parse_args()
 
 # x-axis
 dg = 1
-gross = np.arange(0, args.max, dg)
+gross = np.arange(1, args.max, dg)
 
 # Calculate taxes
 it = IncomeTax.tax(gross)
 ni = NationalInsurance.tax(gross)
 if args.student:
     sf = StudentFinance.tax(gross)
-    areas = [sf, ni, it]
 else:
     sf = 0.
-    areas = [ni, it]
 
 tax = it + ni + sf
 takehome = gross - tax
+
+if args.percent:
+    takehome *= 100./gross
+    tax *= 100./gross
+    it *= 100./gross
+    ni *= 100./gross
+    sf *= 100./gross
+
 areas = [takehome, it, ni]
 labels = ['Take-Home', 'Income Tax', 'National Ins.']
 colors = ['darkgreen', 'darkred', 'sandybrown']
@@ -46,17 +52,29 @@ fig, ax1 = plt.subplots(1)
 y = np.row_stack(areas)
 ax1.stackplot(gross, y, labels=labels, colors=colors)
 
-# Format x- and y-axis to count as 10k, 20k etc.
+x_max = args.max
+
+# Format money axes to count as 10k, 20k, 30k, etc.
 gbp_formatter = FuncFormatter(lambda x, pos: '{:1.0f}k'.format(x*1e-3))
 ax1.xaxis.set_major_formatter(gbp_formatter)
-ax1.yaxis.set_major_formatter(gbp_formatter)
+if args.percent:
+    # Format y axis as 10%, 20%, 30%, etc.
+    y_formatter = FuncFormatter(lambda x, pos: '{:1.0f}%'.format(x))
+    y_max = 100.
+    y_label = 'Percentage of Gross'
+    legend_loc = 4
+else:
+    y_formatter = gbp_formatter
+    y_max = x_max
+    y_label = 'Money [GBP]'
+    legend_loc = 2
 
-ax1.set_xlim([0, args.max])
-ax1.set_ylim([0, args.max])
-ax1.set_xlabel('Income [GBP]')
-ax1.set_ylabel('Money [GBP]')
-ax1.tick_params('y')
-ax1.legend(loc=2)
+ax1.yaxis.set_major_formatter(y_formatter)
+ax1.set_xlim([0, x_max])
+ax1.set_ylim([0, y_max])
+ax1.set_xlabel('Gross Income [GBP]')
+ax1.set_ylabel(y_label)
+ax1.legend(loc=legend_loc)
 
 fig.tight_layout()
 plt.show()

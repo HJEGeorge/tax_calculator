1f334758f246c1728044ec7b342328d275dc667d
JMcQuillanMBAir <johan.mcquillan.13@ucl.ac.uk>
2018-06-22 12:39:16 +0100

    Color console output

--- tax.py
+++ tax.py
@@ -3,8 +3,12 @@
 
 import sys
 from argparse import ArgumentParser
+from colorama import init, Fore
 
 from tax_tools import IncomeTax, NationalInsurance, StudentFinance
+
+# Initialise colorama
+init()
 
 parser = ArgumentParser()
 parser.add_argument('gross', type=float,
@@ -18,32 +22,32 @@ gross = args.gross * 1e3
 take_home = gross
 
 # Initialise output data
-output = [('Gross Income', ' ', gross)]
+output = [('Gross Income', Fore.GREEN, ' ', gross)]
 
 # Apply Income Tax
 it = IncomeTax.tax(gross)
 take_home -= it
-output.append(('Income Tax', '-', it))
+output.append(('Income Tax', Fore.RED, '-', it))
 
 # Apply National Insurance
 ni = NationalInsurance.tax(gross)
 take_home -= ni
-output.append(('National Ins.', '-', ni))
+output.append(('National Ins.', Fore.RED, '-', ni))
 
 # Apply student loan repayments
 if args.student:
     slr = StudentFinance.tax(gross)
-    output.append(('Student Loan', '-', slr))
+    output.append(('Student Loan', Fore.RED, '-', slr))
 else:
     slr = 0.
 take_home -= slr
 
 # Calculate take-home pay
-output.append(('Takehome', ' ', take_home))
-output.append(('Monthly Pay', ' ', take_home / 12.))
-output.append(('Weekly Pay', ' ', take_home / 52.))
+output.append(('Takehome', Fore.GREEN, ' ', take_home))
+output.append(('Monthly Pay', Fore.GREEN, ' ', take_home / 12.))
+output.append(('Weekly Pay', Fore.GREEN, ' ', take_home / 52.))
 
 # Print output
-for name, sign, money in output:
-    sys.stdout.write('{:>15}:   {}{:>12,.2f} £\n'.format(name, sign, money))
+for name, sign, color, money in output:
+    sys.stdout.write('{:>15}:   {}{}{:>12,.2f} £{}\n'.format(name, sign, color, money, Fore.WHITE))
 sys.stdout.flush()

b8d35853cc73664a2c80cf9ae0a327b559b62a19
JMcQuillanMBAir <johan.mcquillan.13@ucl.ac.uk>
2018-06-23 20:37:50 +0100

    Use dicts for string formatting instead of tuple

--- tax.py
+++ tax.py
@@ -10,6 +10,7 @@ from tax_tools import IncomeTax, NationalInsurance, StudentFinance
 # Initialise colorama
 init()
 
+# Parse command line arguments
 parser = ArgumentParser()
 parser.add_argument('gross', type=float,
                     help='Gross income in thousands of £')
@@ -22,32 +23,53 @@ gross = args.gross * 1e3
 take_home = gross
 
 # Initialise output data
-output = [('Gross Income', Fore.GREEN, ' ', gross)]
+output = [{ 'title':    'Gross Income',
+            'amount':   gross,
+            'colour':   Fore.GREEN,
+            'sign':     ' '}]
 
 # Apply Income Tax
 it = IncomeTax.tax(gross)
 take_home -= it
-output.append(('Income Tax', Fore.RED, '-', it))
+output.append({ 'title':    'Income Tax',
+                'amount':   it,
+                'colour':   Fore.RED,
+                'sign':     '-'})
 
 # Apply National Insurance
 ni = NationalInsurance.tax(gross)
 take_home -= ni
-output.append(('National Ins.', Fore.RED, '-', ni))
+output.append({ 'title':    'National Ins.',
+                'amount':   ni,
+                'colour':   Fore.RED,
+                'sign':     '-'})
 
 # Apply student loan repayments
 if args.student:
     slr = StudentFinance.tax(gross)
-    output.append(('Student Loan', Fore.RED, '-', slr))
+    output.append({ 'title':    'Income Tax',
+                    'amount':   slr,
+                    'colour':   Fore.RED,
+                    'sign':     '-'})
 else:
     slr = 0.
 take_home -= slr
 
 # Calculate take-home pay
-output.append(('Takehome', Fore.GREEN, ' ', take_home))
-output.append(('Monthly Pay', Fore.GREEN, ' ', take_home / 12.))
-output.append(('Weekly Pay', Fore.GREEN, ' ', take_home / 52.))
+output.append({ 'title':    'Take-Home Pay',
+                'amount':   take_home,
+                'colour':   Fore.GREEN,
+                'sign':     ' '})
+output.append({ 'title':    'Monthly Pay',
+                'amount':   take_home / 12,
+                'colour':   Fore.GREEN,
+                'sign':     ' '})
+output.append({ 'title':    'Weekly Pay',
+                'amount':   take_home / 52,
+                'colour':   Fore.GREEN,
+                'sign':     ' '})
 
 # Print output
-for name, sign, color, money in output:
-    sys.stdout.write('{:>15}:   {}{}{:>12,.2f} £{}\n'.format(name, sign, color, money, Fore.WHITE))
+for out_dict in output:
+    sys.stdout.write('{title:>15}:   {colour}{sign}{amount:>12,.2f} £{white}\n'.format(white=Fore.WHITE, **out_dict))
 sys.stdout.flush()

bebccad88d24000f9b66772e2bcb75885d813348
JMcQuillanMBAir <johan.mcquillan.13@ucl.ac.uk>
2018-06-28 15:59:08 +0100

    Expand TaxCode and create instantiation function

--- tax_tools.py
+++ tax_tools.py
@@ -61,7 +61,7 @@ class TaxCode(Tax):
     """Class to calculate the overall payments for a collection of income-dependent payments.
     
     Args:
-        tax_brackets (TaxBracket): Taxes to form the overall tax code.
+        *tax_brackets: TaxBracket objects to form the overall tax code.
     """
 
     def __init__(self, *tax_brackets):
@@ -69,10 +69,11 @@ class TaxCode(Tax):
     
     def tax(self, income):
         """Calculate total on a given gross income."""
+        
         total = 0
         for tb in self.tax_brackets:
             total += tb.tax(income)
-            
+    
 
 IncomeTax = TaxBracket([11850, 46350, 150000],
                        [0.20,  0.40,   0.45])
@@ -85,3 +86,39 @@ StudentFinance = TaxBracket([25000],
 
 StatutoryPension = TaxBracket([5876, 45000],
                               [0.03,   0.0])
+
+
+def generate_tax_code(student=False, stat_pension=True, private_pension=None):
+    """Create a TaxCode object from specified conditions.
+    
+    A call with default arguments will include Income Tax, National Insurance, and auto-enrolled
+    Statutory Pension contributions.
+    
+    Args:
+        student (bool, opt.): If true, include student loan repayments.
+        stat_pension (bool, opt.): If true, include Statutory Pension contributions.
+        private_pension (list, opt.): Specify private pension contributions.
+            First argument is the marginal rate.
+            Second argument is the threshold above which this is paid (defaults to 0).
+            If private_pension is specified, Statutory Pension will be ignored.
+    """
+    
+    tax_code = [IncomeTax, NationalInsurance]
+    
+    if student:
+        tax_code.append(StudentFinance)
+    
+    if stat_pension and private_pension is None:
+        tax_code.append(StatutoryPension)
+    
+    if private_pension is not None:
+        rate = [private_pension[0]]
+        
+        try:
+            threshold = [private_pension[1]]
+        except IndexError:
+            threshold = [0]
+        
+        tax_code.append(TaxBracket(rate, threshold))
+
+    return tax_code
